506213a818c91ce3442610cfbcae4fe9e1a9b56d
JaviSede <javisede7@gmail.com>
2025-03-20 10:41:22 +0100

    Initial Commit

---
+++ AnimeTracker/AnimeTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  AnimeTrackerApp.swift
+//  AnimeTracker
+//
+//  Created by Javi Sedeño on 20/3/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AnimeTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AnimeTrackerTests/AnimeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerTests.swift
+//  AnimeTrackerTests
+//
+//  Created by Javi Sedeño on 20/3/25.
+//
+
+import Testing
+@testable import AnimeTracker
+
+struct AnimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
